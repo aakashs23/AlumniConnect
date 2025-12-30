e85d0ddc26e1526cd3bcf484d5c5cc0b3fa6e375
Aakash <Avinash@Aakashs-MacBook-Pro.local>
2025-12-30 23:36:28 +0530

    third ip change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://192.168.0.221:5000/api",
 });
 
 // Automatically attach token if it exists
